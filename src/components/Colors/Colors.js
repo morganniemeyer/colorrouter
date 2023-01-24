aa57ad46982527377b0a83b82edd9965df312e21
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-24 13:49:51 -0600

    navlinks built

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -5,7 +5,9 @@ export default function Colors() {
   let { r, g, b } = useParams();
   return (
     <div style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
-      <h1>RGB</h1>
+      <h1>
+        RGB ({r}, {g}, {b}){' '}
+      </h1>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <NavLink to="/rgb/237/191/198">Baby Pink</NavLink>
+      <NavLink to="/rgb/169/255/247">Celeste</NavLink>
+      <NavLink to="/rgb/148/251/171">Mint Green</NavLink>
+    </header>
+  );
 }
