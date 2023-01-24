c770d67f053e63fb68ad3c648e6ee87d97a98156
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-24 14:30:22 -0600

    colors changing based on color selected

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './Colors.css';
 
 export default function Colors() {
   let { r, g, b } = useParams();
   return (
     <div style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
-      <h1>
-        RGB ({r}, {g}, {b}){' '}
-      </h1>
+      <main>
+        <h1>
+          RGB ({r}, {g}, {b})
+        </h1>
+      </main>
     </div>
   );
 }
