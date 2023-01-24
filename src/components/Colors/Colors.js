62c1c66a51491d5ae40444301b1dc31de89cd44c
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-24 13:11:10 -0600

    color pathing functional

---
+++ src/components/Colors/Colors.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Colors() {
+    let { r, g, b } = useParams();
+  return <div style={ backgroundColor: `rgb(${r}, ${g}, ${b})`}>
+    <h1>RGB</h1>
+  </div>;
+}
