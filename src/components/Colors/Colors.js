4147480fe9dc605e62359960fa7617bd5cb4212a
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-24 13:17:27 -0600

    corrected parsing and missing parentheses

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function Colors() {
-    let { r, g, b } = useParams();
-  return <div style={ backgroundColor: `rgb(${r}, ${g}, ${b})`}>
-    <h1>RGB</h1>
-  </div>;
+  let { r, g, b } = useParams();
+  return (
+    <div style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
+      <h1>RGB</h1>
+    </div>
+  );
 }
