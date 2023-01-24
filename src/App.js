1da6c632f449aa64c6359f1e449df6d77862d7ec
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-24 14:45:29 -0600

    not found and default landing pages added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Colors from './components/Colors/Colors.js';
+import NotFound from './components/NotFound/NotFound.js';
 
 function App() {
   return (
     <>
       <Header />
       <Switch>
+        <Route exact path="/">
+          <Redirect to="/rgb/169/255/247" />
+        </Route>
         <Route exact path="/rgb/:r/:g/:b" component={Colors} />
+        <Route exact path="*" component={NotFound} />
       </Switch>
     </>
   );
